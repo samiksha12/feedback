9db1836790bd2a8108b602ad6e5d636428d2982d
samiksha12 <sapkota.samiksha@gmail.com>
2022-07-03 16:33:06 -0500

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { FRONTEND_DOMAIN } from "../config";
 import {UserContext} from "./context/UserContext"
+import { Link } from "react-router-dom";
 
 function Header({ text, children, headerClass }) {
   const {user,logout} = useContext(UserContext);
   return (
     <header className={`d-flex flex-column ` + headerClass}>
       <div className="d-flex flex-row justify-content-center">
-        <h2 className="align-self-center"><a className="heading" href={FRONTEND_DOMAIN}>{text}</a></h2>
+        <h2 className="align-self-center"><Link to="/"><a className="heading">{text}</a></Link></h2>
         {user?.username && <div className="align-self-center username">{"Hi "+ user?.username +" !!"}</div>}
       </div>
       <div className="d-flex justify-content-end">
