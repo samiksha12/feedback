80afac0bbe5c100ceebc395761d142518b23df5d
samiksha12 <sapkota.samiksha@gmail.com>
2022-07-04 11:09:33 -0500

    working with cors

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-import { BACKEND_DOMAIN_USER,BACKEND_DOMAIN_DATA } from "../config";
+import { BACKEND_DOMAIN_USER, BACKEND_DOMAIN_DATA } from "../config";
 export const getUserData = async () => {
   const resp = await fetch(`${BACKEND_DOMAIN_USER}`);
   const data = await resp.json();
@@ -10,8 +10,10 @@ export const getData = async () => {
   const data = await resp.json();
   return data;
 };
-export const postData = async(data)=>{
-  const rep = await fetch(`${BACKEND_DOMAIN_DATA}`,{
+export const postData = async (data) => {
+  const rep = await fetch(`${BACKEND_DOMAIN_DATA}`, {
+    mode: "cors",
+    credentials: "include",
     method: "POST",
     body: JSON.stringify(data),
     headers: {
@@ -19,19 +21,19 @@ export const postData = async(data)=>{
       "Content-Type": "application/json",
     },
   });
-}
-export const deleteData= async(url,id)=>{
-  const rep = await fetch(`${BACKEND_DOMAIN_DATA}${url}/${id}`,{
-    method:"DELETE"
-  })
-}
-export const putData=async(url,id,data)=>{
-  const rep = await fetch(`${BACKEND_DOMAIN_DATA}${url}/${id}`,{
+};
+export const deleteData = async (url, id) => {
+  const rep = await fetch(`${BACKEND_DOMAIN_DATA}${url}/${id}`, {
+    method: "DELETE",
+  });
+};
+export const putData = async (url, id, data) => {
+  const rep = await fetch(`${BACKEND_DOMAIN_DATA}${url}/${id}`, {
     method: "PUT",
     body: JSON.stringify(data),
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-  })
-}
+  });
+};
